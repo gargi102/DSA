b1e83dd1f8f513a518b6670fb93001d778502769
Gargi_Gupta <guptagargi003@gmail.com>
2025-02-27 11:22:13 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (78.96%) - LeetSync

--- LinkedList/Easy/1013-fibonacci-number/fibonacci-number.java
+++ LinkedList/Easy/1013-fibonacci-number/fibonacci-number.java
@@ -1,11 +1,24 @@
+import java.util.Arrays;
+
 class Solution {
     public int fib(int n) {
-      if(n==0){
-        return 0;
-      }
-      if(n==1){
-        return 1;
-      }
-      return fib(n-1)+fib(n-2);
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int[] arr = new int[n + 1];  
+        Arrays.fill(arr, -1);        
+        arr[0] = 0;
+        arr[1] = 1;
+
+        return helper(arr, n);
+    }
+
+    private int helper(int[] arr, int n) {
+        if (arr[n] != -1) {  
+            return arr[n];
+        }
+
+        arr[n] = helper(arr, n - 1) + helper(arr, n - 2);
+        return arr[n];
     }
 }
